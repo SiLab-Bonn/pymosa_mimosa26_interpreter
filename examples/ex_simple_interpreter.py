13ba3ae01bd53c683444f47ce2a2774607ce90d3
thirono <thirono@example.com>
2016-07-28 15:57:48 +0200

    ENH: exsample

--- examples/ex_simple_interpreter.py
+++ examples/ex_simple_interpreter.py
@@ -3,54 +3,18 @@
 
 import sys,os,time
 import tables
-def img_size(w,h):
-  import matplotlib
-  pltsize=2
-  matplotlib.rcParams["font.size"]=7*pltsize
-  matplotlib.rcParams["legend.fontsize"]="small"
-  matplotlib.rcParams['figure.figsize']=4*pltsize*w,3*pltsize*h
-  matplotlib.rcParams["axes.color_cycle"]=["r","b","g","m","c","y","k","w"]
+import matplotlib
+pltsize=2
+matplotlib.rcParams["font.size"]=7*pltsize
+matplotlib.rcParams["legend.fontsize"]="small"
+matplotlib.rcParams['figure.figsize']=8*pltsize,2.1*pltsize
 
 import matplotlib.pyplot as plt
 import numpy as np
-from numba import njit
 
 from pyBAR_mimosa26_interpreter import simple_interpreter
 
-@njit
-def _mk_hist(hits,hist):
-    for h in hits:
-        if h["plane"]>0 and h["plane"]<=6:
-            hist[h["plane"]-1,h["y"],h["x"]]=hist[h["plane"]-1,h["y"],h["x"]] + 1
-    return hist
-
-def mk_hist(fin,fout,noisy=800,limframe=-1):
-    img_size(2,1.4)
-    hist=np.zeros([6,576,1152])
-    n=100000
-    with tables.open_file(fin) as f:
-        end=len(f.root.Hits)
-        if limframe>0:
-          end=min(end,limframe)
-        start=0
-        while start<end:
-            tmpend=min(end,start+n)
-            hits=f.root.Hits[start:tmpend]
-            hist=_mk_hist(hits,hist)
-            start=start+n
-
-    vmax= np.sort(np.reshape(hist[2,:,:],1152*576))[-noisy]
-    for i in range(6):
-       plt.subplot(230+i+1)
-       plt.imshow(hist[i,:,:],vmax=vmax,aspect="auto")
-       plt.colorbar()
-
-    plt.tight_layout()
-    plt.savefig(fout)
-    return fout
-            
 def mk_plot(fin, fout,limframe=10000):
-    img_size(2,0.7)
     fig,ax=plt.subplots(1,3,sharey=True)
     n=100000
     last=-1
@@ -101,11 +65,30 @@ def mk_plot(fin, fout,limframe=10000):
     fig.savefig(fout)
     return fout
 if __name__=="__main__":
-        fin=sys.argv[-1]
-        fout=fin[:-3]+'.png'
-        fout2=fin[:-3]+'2.png'
-        fout_hit=fin[:-3]+'hits.h5'
-        simple_interpreter.m26_interpreter(fin,fout_hit)
-        print fout_hit
-        print mk_plot(fout_hit,fout,limframe=10000)
-        print mk_hist(fout_hit,fout2,noisy=800)
+    datdir='/sirrush/thirono/testbeam/2016-05-31/mimosa'
+    distdir='/sirrush/silab/Toko/elsa_160531_hits_per_frame'
+    flist=np.sort(os.listdir(datdir))
+    donelist=os.listdir(distdir)
+    for f in flist:
+        if 'test_m26_telescope_scan.h5' not in f:
+            continue
+        if f[:-3]+'.png' in donelist:
+            continue
+        if f in ['18_test_m26_telescope_scan.h5']:
+            continue
+        fin=os.path.join(datdir,f)
+        fout=os.path.join(distdir,f[:-3]+'.png')
+        try:
+            simple_interpreter.m26_interpreter(fin,"hits.h5")
+        except:
+            print "%s has error"%fin
+        print fin
+        try:
+            mk_plot("hits.h5", fout,limframe=10000)
+        except:
+            print "%s has error during plotting"%fin
+            pass
+    os.remove("hits.h5")
+
+
+
