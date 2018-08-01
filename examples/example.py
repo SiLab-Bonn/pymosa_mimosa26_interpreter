05dd793a84a674e0d50a70d5c78ff55d956c96bb
Jens Janssen <janssen@physik.uni-bonn.de>
2018-08-01 22:57:27 +0200

    ENH: fix example, adding converter to testbeam analysis dtype

--- examples/example.py
+++ examples/example.py
@@ -1,137 +1,94 @@
 '''Example how to use the M26 data interpreter. A hit table is created from raw data, additionally events are build using the TLU data words.
     At the end, the hit table is formatted into the correct data format needed for testbeam analysis.
-    The example raw data file was taken with pyBAR, thats why also FE-I4 data is included in the raw data and the event status for the TLU
-    displays unkown_words (FE-I4 words, only M26 and TLU data is expected in the raw data).
 '''
 
 import logging
+import os
+
 import numpy as np
 import tables as tb
 
 from tqdm import tqdm
-from multiprocessing import Pool
+
+from contextlib2 import ExitStack
 
 from pyBAR_mimosa26_interpreter import data_interpreter
 
+
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
 
-
-def analyze_raw_data(raw_data_file, time_reference_file, trigger_data_format):  # Mimosa26 raw data analysis
-    '''Analyze Mimosa26 raw data
-
-        Parameters
-        ----------
-        raw_data_file : pytables file
-            Mimosa26 raw data file
-        time_reference_file : pytables file
-            Time reference interpreted data
-        trigger_data_format : uint
-            Number which indicates the used trigger data format.
-            0: TLU word is trigger number (not supported)
-            1: TLU word is timestamp (not supported)
-            2: TLU word is 15 bit timestamp + 16 bit trigger number
-            Only trigger data format 2 is supported, since the event building requires a trigger timestamp in order to work reliably.
-    '''
-    with data_interpreter.DataInterpreter(raw_data_file=raw_data_file, time_reference_file=time_reference_file, trigger_data_format=trigger_data_format) as raw_data_analysis:
-        raw_data_analysis.create_hit_table = True
-        raw_data_analysis.interpret_word_table()  # interpret raw data
-        # raw_data_analysis.interpret_hit_table()  # build events
+testbeam_analysis_dtype = np.dtype([
+    ('event_number', np.int64),
+    ('frame', np.uint32),
+    ('column', np.uint16),
+    ('row', np.uint16),
+    ('charge', np.uint16)])
 
 
-def process_dut(raw_data_file, time_reference_file, trigger_data_format, transpose=False, frame=False):
-    ''' Process raw data files
-
-        Parameters
-        ----------
-        raw_data_file : pytables file
-            Mimosa26 raw data file
-        time_reference_file : pytables file
-            Time reference interpreted data
-        trigger_data_format : uint
-            Number which indicates the used trigger data format.
-            0: TLU word is trigger number (not supported)
-            1: TLU word is timestamp (not supported)
-            2: TLU word is 15 bit timestamp + 16 bit trigger number
-            Only trigger data format 2 is supported, since the event building requires a trigger timestamp in order to work reliably.
-        transpose : boolean
-            If True, switch column and row index. Default is False
-        frame : boolean
-            If True, store Mimosa26 frame ID. Default is False
-    '''
-    # analyze raw data
-    analyze_raw_data(raw_data_file, time_reference_file, trigger_data_format=trigger_data_format)
-    # format hit table to correct data format for testbeam analysis
-    for plane in range(1, 7):
-        logging.info("Formating hit table of plane %i...", plane)
-        format_hit_table(input_file=raw_data_file[:-3] + '_interpreted_event_build_aligned_plane_%i.h5' % plane,
-                         output_file=raw_data_file[:-3] + '_aligned_plane_%i.h5' % plane,
-                         transpose=transpose,
-                         frame=frame,
-                         chunk_size=1000000)
-
-
-def format_hit_table(input_file, output_file, transpose=False, frame=False, chunk_size=1000000):
+def format_hit_table(input_filename, output_filenames=None, chunk_size=1000000):
     ''' Selects and renames important columns for test beam analysis and stores them into a new file.
 
-        Parameters
-        ----------
-        input_file : pytables file
-        output_file : pytables file
-        transpose : boolean
-            If True, column and row index are transposed. Default is False.
-        frame : boolean
-            If True, store frame id of M26 data, else set to zero since usually not needed. Default is False.
-        chunksize : uint
+    Parameters
+    ----------
+    input_filename : string
+        Filename of the input interpreted data file.
+    output_filenames : list
+        Filenames of the output interpreted data files.
+        If None, the filenames will be generated.
+    chunk_size : uint
+        Chunk size of the data when reading from file.
+
+    Returns
+    -------
+    output_filenames : list
+        Filenames of the output interpreted data files.
     '''
-    with tb.open_file(input_file, 'r') as in_file_h5:
-        m26_hit_table = in_file_h5.root.Hits[:]
-        n_m26 = m26_hit_table.shape[0]
-        with tb.open_file(output_file, 'w') as out_file_h5:
-            description = np.dtype([('event_number', np.int64), ('frame', np.uint8),
-                                    ('column', np.uint16), ('row', np.uint16), ('charge', np.uint16)])
-            hit_table_out = out_file_h5.create_table(where=out_file_h5.root,
-                                                     name='Hits',
-                                                     description=description,
-                                                     title='Selected hits for test beam analysis',
-                                                     filters=tb.Filters(complib='blosc', complevel=5, fletcher32=False))
+    if output_filenames is None:
+        output_filenames = [(os.path.splitext(input_filename)[0] + '_telescope' + str(plane + 1) + '.h5') for plane in range(6)]
+    with tb.open_file(filename=input_filename, mode='r') as in_file_h5:
+        input_hits_table = in_file_h5.root.Hits
+        with ExitStack() as output_files_stack:
+            out_files_h5 = [output_files_stack.enter_context(tb.open_file(filename=filename, mode='w')) for filename in output_filenames]
+            output_hits_tables = []
+            for out_file_h5 in out_files_h5:
+                output_hits_table = out_file_h5.create_table(
+                    where=out_file_h5.root,
+                    name='Hits',
+                    description=testbeam_analysis_dtype,
+                    title='Hits for test beam analysis',
+                    filters=tb.Filters(
+                        complib='blosc',
+                        complevel=5,
+                        fletcher32=False))
+                output_hits_tables.append(output_hits_table)
 
-            for i in tqdm(range(0, n_m26, chunk_size)):
-                m26_data = m26_hit_table[i:i + chunk_size]
-                hits_formatted = np.zeros(shape=m26_data.shape[0], dtype=description)
+            for read_index in tqdm(range(0, input_hits_table.nrows, chunk_size)):
+                hits_data = input_hits_table.read(read_index, read_index + chunk_size)
+                for plane, output_hits_table in enumerate(output_hits_tables):
+                    selected_hits = (hits_data['plane'] == plane)
+                    hits_data_formatted = np.zeros(shape=np.count_nonzero(selected_hits), dtype=testbeam_analysis_dtype)
+                    # Format data for testbeam analysis
+                    hits_data_formatted['event_number'] = hits_data[selected_hits]['event_number']
+                    hits_data_formatted['frame'] = 0
+                    hits_data_formatted['column'] = hits_data[selected_hits]['column'] + 1
+                    hits_data_formatted['row'] = hits_data[selected_hits]['row'] + 1
+                    hits_data_formatted['charge'] = 0
+                    # Append data to table
+                    output_hits_table.append(hits_data_formatted)
+                    output_hits_table.flush()
 
-                hits_formatted['event_number'] = m26_data['event_number']
-
-                if frame:
-                    hits_formatted['frame'] = m26_data['frame']
-                else:
-                    hits_formatted['frame'] = 0
-
-                if transpose:
-                    hits_formatted['column'] = m26_data['row'] + 1
-                    hits_formatted['row'] = m26_data['column'] + 1
-                else:
-                    hits_formatted['column'] = m26_data['column'] + 1
-                    hits_formatted['row'] = m26_data['row'] + 1
-
-                hits_formatted['charge'] = 1
-
-                hit_table_out.append(hits_formatted)
-                hit_table_out.flush()
+    return output_filenames
 
 
 if __name__ == "__main__":
-    # specify M26 raw data files
-    raw_data_files = [r'./anemone_raw_data.h5'
-                      ]
-    # specify aligned time reference data files
-    time_reference_files = [r'./time_reference_interpreted_data.h5'
-                            ]
-
-    trigger_data_format = 2  # use combined mode for raw data interpretation, this is the only supported format
-
-    # pool = Pool()
-    # results = pool.map(process_dut, raw_data_files, time_reference_file, trigger_data_format)
-    for raw_data_file, time_reference_file in zip(raw_data_files, time_reference_files):
-        process_dut(raw_data_file, time_reference_file, trigger_data_format)
-    # pool.close()
-    # pool.join()
+    # Input interpreted filename
+    raw_data_file = 'anemone_raw_data.h5'
+    # Output interpreted filename
+    analyzed_data_file = 'anemone_interpreted.h5'
+    with data_interpreter.DataInterpreter(raw_data_file=raw_data_file, analyzed_data_file=analyzed_data_file) as data_interpreter:
+        data_interpreter.create_occupancy_hist = True
+        data_interpreter.create_error_hist = True
+        data_interpreter.create_hit_table = True
+        data_interpreter.interpret_word_table()  # interpret raw data
+    # Convert format
+    format_hit_table(input_filename=analyzed_data_file)
