dd40ea48484a0922cff449b74f29da900242d4b0
Jens Janssen <janssen@physik.uni-bonn.de>
2019-10-11 11:43:33 +0200

    MAINT: fix project specific package settings, license

--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ with open('requirements.txt') as f:
 setup(
     name='pyBAR_mimosa26_interpreter',
     version=version,
-    description='This package can be used to interpred raw data from the Mimosa 26 telescope taken with the readout framework pyBAR. It also contains histogramming functions. The interpretation uses numba JIT to increase the speed.',
+    description='Interpreter for Mimosa26 raw data recorded with pymosa DAQ',
     url='https://github.com/SiLab-Bonn/pyBAR_mimosa26_interpreter',
-    license='GNU LESSER GENERAL PUBLIC LICENSE Version 2.1',
+    license='BSD 3-Clause License',
     long_description='',
     author=author,
     maintainer=author,
@@ -25,8 +25,7 @@ setup(
     install_requires=install_requires,
     packages=find_packages(),
     include_package_data=True,  # accept all data files and directories matched by MANIFEST.in or found in source control
-    package_data={'': ['README.*', 'VERSION', 'LICENSE.txt'], 'docs': ['*'], 'examples': ['*']},
-    keywords=['mimosa26', 'test beam', 'pixel'],
+    keywords=['mimosa26', 'test-beam', 'pixel', 'telescope'],
     python_requires='>=2.7',
     platforms='any'
 )
